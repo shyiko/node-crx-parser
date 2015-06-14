78562473cdb89c1245b59033c9db4d7ac65c1ba5
Stanley Shyiko <stanley.shyiko@gmail.com>
2015-06-14 15:26:28 -0700

    Fixed incompatibility with V8 used in Node 0.12.X (https://code.google.com/p/v8/issues/detail?id=3578)

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
-function readUnsignedInt(arr, offset) {
-    return (arr[offset + 3] << 24) | (arr[offset + 2] << 16) |
-        (arr[offset + 1] << 8) | arr[offset];
-}
-
 module.exports = function (buff, cb) {
-    var view = new Uint8Array(buff);
+  if (buff.readUInt32LE(0) !== 0x34327243 /* Cr24 */) {
+    return cb(new Error('Unexpected CRX magic number')), undefined;
+  }
 
-    if (readUnsignedInt(view, 0) !== 0x34327243 /* Cr24 */) {
-        return cb(new Error('Unexpected CRX magic number')), undefined;
-    }
+  if (buff.readUInt32LE(4) !== 2) {
+    return cb(new Error('Unexpected CRX version')), undefined;
+  }
 
-    if (readUnsignedInt(view, 4) !== 2) {
-        return cb(new Error('Unexpected CRX version')), undefined;
-    }
+  var publicKeyLength = buff.readUInt32LE(8);
+  var signatureLength = buff.readUInt32LE(12);
+  var metaOffset = 16;
+  var publicKey = new Buffer(buff.slice(metaOffset,
+    metaOffset + publicKeyLength)).toString('base64');
+  var signature = new Buffer(buff.slice(metaOffset + publicKeyLength,
+    metaOffset + publicKeyLength + signatureLength)).toString('base64');
 
-    var publicKeyLength = readUnsignedInt(view, 8);
-    var signatureLength = readUnsignedInt(view, 12);
-    var metaOffset = 16;
-    var publicKey = new Buffer(view.slice(metaOffset,
-        metaOffset + publicKeyLength)).toString('base64');
-    var signature = new Buffer(view.slice(metaOffset + publicKeyLength,
-        metaOffset + publicKeyLength + signatureLength)).toString('base64');
-
-    cb(null, {
-        header: {
-            publicKey: publicKey,
-            signature: signature
-        },
-        body: buff.slice(metaOffset + publicKeyLength + signatureLength)
-    });
+  cb(null, {
+    header: {
+      publicKey: publicKey,
+      signature: signature
+    },
+    body: buff.slice(metaOffset + publicKeyLength + signatureLength)
+  });
 };
