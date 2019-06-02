20b78baecb845eac64db606dfde9eb6c7394899f
Sean <sean@bearcla.ws>
2019-06-02 16:28:31 -0400

    successfully pull ZIP body from CRX3 files

--- src/index.js
+++ src/index.js
@@ -8,19 +8,33 @@ module.exports = function (buff, cb) {
     return cb(new Error('Unexpected CRX version')), undefined;
   }
 
-  var publicKeyLength = buff.readUInt32LE(8);
-  var signatureLength = buff.readUInt32LE(12);
-  var metaOffset = 16;
-  var publicKey = new Buffer(buff.slice(metaOffset,
-    metaOffset + publicKeyLength)).toString('base64');
-  var signature = new Buffer(buff.slice(metaOffset + publicKeyLength,
-    metaOffset + publicKeyLength + signatureLength)).toString('base64');
+  if (crxVersion === 2) {
+    var publicKeyLength = buff.readUInt32LE(8);
+    var signatureLength = buff.readUInt32LE(12);
+    var metaOffset = 16;
+    var publicKey = new Buffer(buff.slice(metaOffset,
+      metaOffset + publicKeyLength)).toString('base64');
+    var signature = new Buffer(buff.slice(metaOffset + publicKeyLength,
+      metaOffset + publicKeyLength + signatureLength)).toString('base64');
+  
+    cb(null, {
+      header: {
+        publicKey: publicKey,
+        signature: signature
+      },
+      body: buff.slice(metaOffset + publicKeyLength + signatureLength)
+    });  
+  } else if (crxVersion === 3) {
+    var headerLength = buff.readUInt32LE(8);
+    var metaOffset = 12;
+    var rawHeader = new Buffer(buff.slice(metaOffset,
+      metaOffset + headerLength));
+    
+    cb(null, {
+      header: null,
+      body: buff.slice(metaOffset + headerLength)
+    });  
+  
+  }
 
-  cb(null, {
-    header: {
-      publicKey: publicKey,
-      signature: signature
-    },
-    body: buff.slice(metaOffset + publicKeyLength + signatureLength)
-  });
 };
